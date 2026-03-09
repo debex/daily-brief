0f50fe7e0ab7c403e0a63bb270e8c2f00e00adb3
pdotk <pam@automattic.com>
2026-03-09 11:26:04 -0500

    Update daily_brief.py

--- daily_brief.py
+++ daily_brief.py
@@ -28,7 +28,6 @@ def fetch_linear_issues():
                         state: {
                             type: { in: ["started", "unstarted"] }
                         }
-                        assignee: { isMe: { eq: true } }
                     }
                     orderBy: updatedAt
                     first: 50
